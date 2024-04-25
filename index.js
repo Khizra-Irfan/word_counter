a051726a099485f6d6012d9e2e662ac065fab396
Khizra-Irfan <khizrairfan53@gmail.com>
2024-04-26 00:52:54 +0500

    Making of Word-Counter

---
+++ index.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const wordCounter = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word: "
+    }
+]);
+//.split method is used to make an array and separate all the words not letters.
+//.trim method is used for removing white spaces from the start and end of the sentence but not from between the words.
+const words = wordCounter.Sentence.trim().split(" ");
+console.log(words);
+console.log(`Your sentence word count is ${words.length}`);
